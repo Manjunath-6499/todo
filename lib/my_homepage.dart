27df121abeb90665a41d5997da8283020e694dde
Manjunath-6499 <Manjumadesh16@gmail.com>
2024-02-18 18:17:22 +0530

    todo app is created

---
+++ lib/my_homepage.dart
@@ -0,0 +1,35 @@
+import "package:flutter/material.dart";
+import "package:todo_app/util/todo_list.dart";
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow.shade200,
+      appBar: AppBar(
+        elevation: 0,
+        title: const Center(
+          child: Text(
+            "TODO",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.yellow.shade500,
+      ),
+      body: ListView(
+        children: const [
+          TOdoTile(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/util/todo_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TOdoTile extends StatefulWidget {
+  const TOdoTile({super.key});
+
+  @override
+  State<TOdoTile> createState() => _TOdoTileState();
+}
+
+class _TOdoTileState extends State<TOdoTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
